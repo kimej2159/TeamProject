baed2acf14ee4b9a7e4eb0323a5e2b379f05fb34
kimhyunhee900 <khh4477@naver.com>
2023-04-17 13:12:03 +0900

    커뮤니티 작업시작..

---
+++ pepsi/src/main/java/board/BoardService.java
@@ -0,0 +1,18 @@
+package board;
+
+public interface BoardService {
+
+	
+	//CRUD(Create, Read, Updata, Delete)
+	// 새글등록
+	// 목록조회
+	// 글수정저장
+	// 글삭제
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ pepsi/src/main/java/com/hanul/pepsi/BoardController.java
@@ -0,0 +1,18 @@
+package com.hanul.pepsi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	
+	/*
+	 * //게시판화면요청
+	 * 
+	 * @RequestMapping("/list.bo") public String list() {
+	 * 
+	 * return "board/list"; }
+	 */
+
+}
